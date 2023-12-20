0e4725140a8cb374872f5688bc2c729a7590b1db
rajendra.rakha <rajendra.rakha@megafinance.co.id>
2023-12-20 10:10:58 +0700

    Feat : Add Tailwind And React Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="w-full h-screen bg-red-300 flex flex-col">
+      <header className="bg-red-500 w-full h-12 fixed top-0 z-10 flex justify-center">
+        <div className="h-full w-full max-w-6xl bg-white flex items-center relative">
+          <div className="md:hidden flex items-center w-full max-w-[4rem] h-full left-0 justify-center absolute">
+            <GiHamburgerMenu />
+          </div>
+          <div>
+            <h1 className="text-lg text-orange-500">Test</h1>
+          </div>
+        </div>
       </header>
+
+      <main className="bg-red-500 mt-16 w-full h-[100vh] flex justify-center">
+        <div className="bg-white h-full w-full max-w-6xl relative">
+          <div className="absolute bg-red-950 left-0 h-full w-48">
+            <h1 className="text-lg text-orange-500">Sidebar</h1>
+          </div>
+
+          <section className="ml-48 bg-red-200">
+            <h1 className="text-lg text-orange-500">Test</h1>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
