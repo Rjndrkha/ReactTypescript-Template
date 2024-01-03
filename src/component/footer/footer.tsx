2b5f6246edd6ef0fca5caaf8c3b9ae246180a4cf
rajendra.rakha <rajendra.rakha@megafinance.co.id>
2024-01-03 09:38:27 +0700

    Feat : Add Footer

---
+++ src/component/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header className="z-20 w-full h-12 flex justify-center">
+      <div className="h-full w-full max-w-6xl bg-red-500 items-center relative">
+        <h1>Footer</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Footer;
