fe96d495bb0fc10a996e29be654cdb0b718d238b
Rajendra Rakha <rajendra.rakha29@gmail.com>
2024-01-24 14:57:44 +0700

    FIX : Logic Menu Optimization

--- src/component/menu/menu.tsx
+++ src/component/menu/menu.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { Menu } from "antd";
-import { MenuProps } from "antd/lib/menu";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Menu, MenuProps } from "antd";
+import { Link, useLocation } from "react-router-dom";
 import { IRoute, menuItems } from "./listMenu";
 
 type MenuItem = Required<MenuProps>["items"][number];
@@ -37,18 +36,56 @@ const getMenuItems = (routes: IRoute[] | undefined): MenuItem[] => {
 };
 
 const SideMenu: React.FC = () => {
+  const location = useLocation();
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+  const [openKeys, setOpenKeys] = useState<string[]>([]);
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    const foundItem = menuItems.find((item) => {
+      if (item.link === currentPath) {
+        return true;
+      } else if (item.children) {
+        return item.children.some((child) => child.link === currentPath);
+      }
+      return false;
+    });
+
+    if (foundItem) {
+      if (currentPath !== foundItem.link) {
+        foundItem.children?.forEach((child) => {
+          if (child.link === currentPath) {
+            setSelectedKeys([child.key || ""]);
+          }
+        });
+        setOpenKeys([foundItem.key || ""]);
+      }
+
+      if (currentPath === foundItem.link) {
+        setSelectedKeys([foundItem.key || ""]);
+        setOpenKeys([foundItem.key || ""]);
+      }
+    }
+  }, [location.pathname]);
+
   const items: MenuItem[] = getMenuItems(menuItems);
+
+  useEffect(() => {
+    console.log(openKeys);
+  }, [openKeys]);
 
   return (
     <>
-      {/* <img src={} alt="logo" className="hidden md:block" /> */}
-      <Menu
-        style={{ backgroundColor: "none" }}
-        mode="inline"
-        items={items}
-        defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["Home"]}
-      />
+      {/* <img src={ASSETS.LOGOMF} alt="logo" className="hidden md:block" /> */}
+      {openKeys.length > 0 && (
+        <Menu
+          style={{ backgroundColor: "none" }}
+          mode="inline"
+          items={items}
+          selectedKeys={selectedKeys}
+          defaultOpenKeys={openKeys}
+        />
+      )}
     </>
   );
 };
